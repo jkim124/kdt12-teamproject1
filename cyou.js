671690d6df8cea8cd509bd0fe8215930dbb9cdba
jykim <ruah1234@naver.com>
2025-07-30 19:35:09 +0900

    FIX index 페이지 video 페이지

--- cyou.js
+++ cyou.js
@@ -4,8 +4,10 @@ const texta = document.querySelector("#commenttext");
 
 switch (loc) {
   case "?video=1":
+    // document.querySelector("#ply").src =
+    //   "https://www.youtube.com/embed/CP8jRf1XC48?autoplay=1&mute=1";
     document.querySelector("#ply").src =
-      "https://www.youtube.com/embed/8Cx3Glnj59o?si=siwCOKnHtHQekE_p";
+      "https://www.youtube.com/embed/CP8jRf1XC48?autoplay=1&mute=0";
     const videoinfo = document.querySelector("#info");
     const videoinfofo = document.querySelector("#infotext");
     videoinfo.classList.add("infoblock");
@@ -13,13 +15,59 @@ switch (loc) {
     break;
   case "?video=2":
     document.querySelector("#ply").src =
-      "https://www.youtube.com/embed/AI0MS-3D_co?si=n2CJoKZFUKDfLTIN";
+      "https://www.youtube.com/embed/BsZAsGu_tnM?autoplay=1&mute=0";
     const videoinfo2 = document.querySelector("#info2");
     const videoinfofo2 = document.querySelector("#infotext2");
     videoinfo2.classList.add("infoblock");
     videoinfofo2.classList.add("infoblock");
     break;
   case "?video=3":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/4iA__wS4gjU?autoplay=1&mute=0";
+    const videoinfo3 = document.querySelector("#info3");
+    const videoinfofo3 = document.querySelector("#infotext3");
+    videoinfo3.classList.add("infoblock");
+    videoinfofo3.classList.add("infoblock");
+    break;
+  case "?video=4":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/QJAewX4fnPc?autoplay=1&mute=0";
+    const videoinfo4 = document.querySelector("#info4");
+    const videoinfofo4 = document.querySelector("#infotext4");
+    videoinfo4.classList.add("infoblock");
+    videoinfofo4.classList.add("infoblock");
+    break;
+  case "?video=5":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/BsZAsGu_tnM?autoplay=1&mute=0";
+    const videoinfo5 = document.querySelector("#info5");
+    const videoinfofo5 = document.querySelector("#infotext5");
+    videoinfo5.classList.add("infoblock");
+    videoinfofo5.classList.add("infoblock");
+    break;
+  case "?video=6":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/BsZAsGu_tnM?autoplay=1&mute=0";
+    const videoinfo6 = document.querySelector("#info6");
+    const videoinfofo6 = document.querySelector("#infotext6");
+    videoinfo6.classList.add("infoblock");
+    videoinfofo6.classList.add("infoblock");
+    break;
+  case "?video=7":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/BsZAsGu_tnM?autoplay=1&mute=0";
+    const videoinfo7 = document.querySelector("#info7");
+    const videoinfofo7 = document.querySelector("#infotext7");
+    videoinfo7.classList.add("infoblock");
+    videoinfofo7.classList.add("infoblock");
+    break;
+  case "?video=8":
+    document.querySelector("#ply").src =
+      "https://www.youtube.com/embed/BsZAsGu_tnM?autoplay=1&mute=0";
+    const videoinfo8 = document.querySelector("#info8");
+    const videoinfofo8 = document.querySelector("#infotext8");
+    videoinfo8.classList.add("infoblock");
+    videoinfofo8.classList.add("infoblock");
     break;
   default:
     console.log("입력값이 없어요");
@@ -29,15 +77,13 @@ function reload() {
   (location || window.location || document.location).reload();
   console.log("리로드!!");
 }
-
-const profilebutton = document.getElementById("profile_bt");
 const profilehead = document.getElementById("profile_head");
-
-profilebutton.addEventListener("click", function () {
-  if (profilehead.style.display === "none") {
-    profilehead.style.display = "block";
-  } else {
+const profilebutton = document.getElementById("profile_bt");
+profilebutton.addEventListener("change", function () {
+  if (this.checked) {
     profilehead.style.display = "none";
+  } else {
+    profilehead.style.display = "block";
   }
 });
 
@@ -52,10 +98,21 @@ try {
     }
   });
 } catch (error) {
-  console.error("메인페이지에선 동작안함"); // 에러 메시지 출력
+  console.error("텍스트에어리어 확장 메인페이지에선 동작안함"); // 에러 메시지 출력
 }
 
-function commentcnt() {}
+try {
+  texta.addEventListener("focus", function () {
+    this.placeholder = "";
+  });
+  texta.addEventListener("blur", function () {
+    if (this.value === "") {
+      texta.placeholder = "댓글 추가...";
+    }
+  });
+} catch (error) {
+  console.error("플레이스홀더리셋 메인페이지에선 동작안함"); // 에러 메시지 출력
+}
 
 function commentsb() {
   const savetext = document.querySelector("#commenttext").value;
@@ -77,9 +134,12 @@ function commentsb() {
     "</p>" +
     "</li>" +
     document.querySelector("#commentlist").innerHTML;
+  texta.placeholder = "댓글 추가...";
 }
+
 function refresharea() {
   document.querySelector("#commenttext").value = "";
   document.getElementById("combt").disabled = true;
   document.activeElement.blur();
+  texta.placeholder = "댓글 추가...";
 }
